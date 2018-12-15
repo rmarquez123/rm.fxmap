b61cb926e581d0626d96d6fdbeeaa01ceae8df5c
rmarquez123 <rmarquez123@gmail.com>
2018-12-14 19:07:29 -0800

    created tests.

---
+++ src/test/java/com/rm/fxmap/CreateMapTest.java
@@ -0,0 +1,46 @@
+package com.rm.fxmap;
+
+import com.rm.panzoomcanvas.FxCanvas;
+import com.rm.springjavafx.FxmlInitializer;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author rmarquez
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/spring/main.xml"})
+public class CreateMapTest {
+
+  @Autowired
+  ApplicationContext appContext;
+  @Autowired
+  FxmlInitializer fxmlInitializer;
+  
+  @Autowired
+  FxCanvas map;
+
+  @BeforeClass
+  public static void setUp() throws InterruptedException {
+    JavaFxTest.initToolkit();
+  }
+
+  @Test
+  public void test1() {
+    System.out.println("testing");
+    this.fxmlInitializer.load(this.appContext);
+    Assert.assertNotNull("map not null", this.map);
+    Assert.assertEquals("number of layers", 3, this.map.getContent().getLayers().size());
+    Assert.assertNotNull("Parent is set", this.map.parentProperty().getValue());
+    Assert.assertEquals("Parent is set", "mapstackpane",this.map.parentProperty().getValue().getId());
+    
+    System.out.println("testing");
+  }
+}
---
+++ src/test/java/com/rm/fxmap/JavaFxTest.java
@@ -0,0 +1,26 @@
+package com.rm.fxmap;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import javafx.embed.swing.JFXPanel;
+import javax.swing.SwingUtilities;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author rmarquez
+ */
+public class JavaFxTest {
+
+  @BeforeClass
+  public static void initToolkit() throws InterruptedException {
+    final CountDownLatch latch = new CountDownLatch(1);
+    SwingUtilities.invokeLater(() -> {
+      JFXPanel jfxPanel = new JFXPanel(); 
+      latch.countDown();
+    });
+    if (!latch.await(5L, TimeUnit.SECONDS)) {
+      throw new ExceptionInInitializerError();
+    }
+  }
+}
